cbd2c0249c4dec4eaeef5eae00453e382dfd7de3
Your Name <you@example.com>
2022-02-17 10:50:16 -0500

    updated about me

--- index.php
+++ index.php
@@ -47,8 +47,8 @@ $instagram = new Instagram(array(
 			<a class="navbar-brand vw-100" href=index.html>KELSEY DAMIER</a>
 		</nav>
 		<span id="intro" class="d-flex flex-column px-2">
-			<p>Pixel perfect design and illustration</p>
-			<p>*pixie, my bad :)</p>
+		<p>Pixel perfect design and illustration</p>
+			<p>*pixie, my bad 🧚🏽‍♀️</p>
 			<a class="align-self-end" href=about.html><u>read more</u></a>
 		</span>
 	</header>
